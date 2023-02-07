bab35b754b5bf83ac688a3510aeaa92e288752b8
Maxime Chemenda <maxime.chemenda@gmail.com>
2023-02-07 01:13:18 +0000

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+      colors: {
+          "grey": "#43465C",
+          "white": "#FFFFFF",
+          "background": "#181A25",
+          "section-background": "#151823",
+          "gradient-left": "#AF53FF",
+          "gradient-right": "#6EACFE"
+      }
+    },
+    plugins: [],
+  }
