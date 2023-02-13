96752fbe5391f6fc7b54cda6e63cb0e2e63e3235
Maxime Chemenda <maxime.chemenda@gmail.com>
2023-02-13 17:21:21 +0000

    refactor: Add constants file for multi-select and refactor component

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,64 +1,78 @@
-import React, { CSSProperties } from 'react';
-
+import React from 'react';
 import Select from 'react-select';
 
+// Tailwind imports (to reference colors variables defined in the tailwind config file)
 import tailwindConfig from '../tailwind.config';
 import resolveConfig from 'tailwindcss/resolveConfig'
 
+// Constants
+import {SENTIMENT_OPTIONS} from "../constants"
+
+// Tailwind Config setup
 const fullConfig = resolveConfig(tailwindConfig)
 const tailwindColors: any = fullConfig.theme?.colors
 
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
-
-  const customStyles = {
+// Styles to style the react-select component
+const customStyles = {
+    // Each individual option once the menu is opened
     option: (defaultStyles: any, state: any) => ({
-      ...defaultStyles,
-      color: state.isSelected ? "#212529" : "#fff",
-      backgroundColor: state.isSelected ? "#a0a0a0" : "#212529",
-      padding: "15px",
-      margin: 0
+        ...defaultStyles,
+        color: state.isSelected ? "#212529" : "#fff",
+        backgroundColor: state.isSelected ? "#a0a0a0" : "#212529",
+        padding: "15px",
+        margin: 0
     }),
 
+    // The outer component
     control: (defaultStyles: any) => ({
-      ...defaultStyles,
-      backgroundColor: "transparent",
-      padding: "4px",
-      border: "1px solid #303241",
-      borderRadius: "10px"
+        ...defaultStyles,
+        backgroundColor: "transparent",
+        padding: "4px",
+        border: "1px solid #303241",
+        borderRadius: "10px"
     }),
 
+    // Each individual tag (which is shown once an option is selected)
     multiValue: (defaultStyles: any) => ({
         ...defaultStyles,
         backgroundColor: tailwindColors["tag-bg-color"],
         padding: "2px",
         borderRadius: "5px"
-      }),
+        }),
 
-      multiValueLabel: (defaultStyles: any) => ({
+        // The value inside the tag
+        multiValueLabel: (defaultStyles: any) => ({
     ...defaultStyles,
         color: "tag-text-color",
         padding: "2px"
     }),
 
-      
+    // The menu showing all options once user clicks on "Select"
     menu: (defaultStyles: any) => ({
         ...defaultStyles,
         backgroundColor: tailwindColors["background"],
         padding: 0,
         margin: 0
-      }),
+        }),
 
-    singleValue: (defaultStyles: any) => ({ ...defaultStyles, color: "red", fontColor: "red" }),
-  };
+    singleValue: (defaultStyles: any) => ({ ...defaultStyles, color: "#fff"}),
+};
 
-  export const SelectComponent: React.FC<{}> = () => {
+export const SelectComponent: React.FC<{}> = () => {
     return (
-      <div className="text-tag-text-color text-sm">
-        <Select isMulti options={options} styles={customStyles} />
+        <div className="text-sm">
+            <p className="text-grey">
+                Sentiment
+            </p> 
+
+            <div className="text-tag-text-color">
+                <Select 
+                    isMulti 
+                    options={SENTIMENT_OPTIONS} 
+                    styles={customStyles} 
+                    placeholder="Any"
+                />
+            </div>
         </div>
     )
 }
--- constants.js
+++ constants.js
@@ -1,1 +1,5 @@
-// export const Sentiment
+export const SENTIMENT_OPTIONS = [
+    { value: 'positive', label: 'Positive' },
+    { value: 'negative', label: 'Negative' },
+    { value: 'neutral', label: 'Neutral' }
+]
